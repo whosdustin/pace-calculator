cd4a4ed8362958be8e03b8a39640b95bc186b0c8
Dustin Delatore <dustin@delatore.me>
2018-12-03 14:32:56 -0500

    Refactor ShowDistance

--- src/components/ShowDistance.vue
+++ src/components/ShowDistance.vue
@@ -31,32 +31,29 @@ export default {
     }
   },
   methods: {
+    convertToK(dist) {
+      const value = this.units.find(u => u.value === this.currentUnit).toK || 1
+      return dist / value
+    },
+    convertFromK(dist) {
+      const value = this.units.find(u => u.value === this.newUnit).toK || 1
+      return dist * value
+    },
     convertDistance(event) {
       let result
       let distance = this.distance
-      let unitFromK = this.units.find(u => u.value === event.target.value).toK || 1
-      let unitToK = this.units.find(u => u.value === this.currentUnit).toK || 1
-      let convertToK = (dist, val) => dist / val
-      let convertFromK = (dist, val) => dist * val
 
-      if (event.target.value === 'k') {
-        result = convertToK(distance, unitToK)
-        this.distance = result.toFixed(1)
-        this.currentUnit = event.target.value
-        return
+      if (this.newUnit === 'k') {
+        result = this.convertToK(distance)
+      } else if (this.currentUnit === 'k') {
+        result = this.convertFromK(distance)
+      } else {
+        // Convert to k then from k to new unit
+        result = this.convertFromK(this.convertToK(distance))
       }
 
-      if (this.currentUnit === 'k') {
-        result = convertFromK(distance, unitFromK)
-        this.distance = result.toFixed(1)
-        this.currentUnit = event.target.value
-        return
-      }
-
-      // Convert to k then from k to new unit
-      result = convertFromK(convertToK(distance, unitToK), unitFromK)
       this.distance = result.toFixed(1)
-      this.currentUnit = event.target.value
+      this.currentUnit = this.newUnit
     }
   }
 }
